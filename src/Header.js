0fc227a9c1f147c3833ad99f3c3c825a8902739e
cje444 <cje444@naver.com>
2021-04-15 14:53:14 -0400

    글쓰기 버튼 구현

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,20 @@
 import React from "react";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Button from "react-bootstrap/Button";
+import { withRouter } from "react-router-dom";
 
-const Header = (props) => {
+const Header = ({ history }) => {
   return (
     <Jumbotron>
       <h1>게시판 만들기</h1>
       <p>아주 간단한 게시판을 만들어봅니다.</p>
       <p>
-        <Button variant='primary'>글쓰기</Button>
+        <Button onClick={() => history.push("/write")} variant='primary'>
+          글쓰기
+        </Button>
       </p>
     </Jumbotron>
   );
 };
 
-export default Header;
+export default withRouter(Header);
